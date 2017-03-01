ca5acc0500cbb5e9457e2e82d18a508d60bf8eca
Devon Peterson <devon@devonpeterson.io>
2017-03-01 01:01:09 -0800

    so georedis does work, but the reply is crap

--- src/world.js
+++ src/world.js
@@ -3,15 +3,24 @@
  */
 
 // Includes
-const redis = require('./redis/helper.js');
+const myredis = require('./redis/helper.js');
+var redis = require("redis"),
+    client = redis.createClient();
+var geo = require('georedis').initialize(client);
 
 // Generates a new world
 const generate = function() {
     // Split the world up into quadratic grids
     // TODO more than one resolution
-    console.log("hello");
-    redis.initWorld(function(grid) {
-        console.log(grid);
+    //console.log("hello");
+    myredis.initWorld(function(grid) {
+
+        geo.location('Toronto', function(err, location){
+            if(err) console.error(err);
+            else console.log('Location for Toronto is: ', location.latitude, location.longitude)
+        });
+
+        //console.log(grid);
             /*.forEach(function(cell) {
             console.log(cell);
         });*/
